8e5858f7317f26c6331a7861fe29ca239d9356b2
aamotta0 <aamotta0>
2024-01-12 18:23:23 -0500

    First Commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Button = () => (
+  <button type='button' className={`flex items-center py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px] mt-10`}>
+    Get Started
+  </button>
+  );
+
+
+export default Button
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from './Billing.jsx'
+import Business from './Business.jsx'
+import Button from './Button.jsx'
+import CardDeal from './CardDeal.jsx'
+import Clients from './Clients.jsx'
+import CTA from './CTA.jsx'
+import FeedbackCard from './FeedbackCard.jsx'
+import Footer from './Footer.jsx'
+import GetStarted from './GetStarted.jsx'
+import Hero from './Hero.jsx'
+import Navbar from './Navbar.jsx'
+import Stats from './Stats.jsx'
+import Testimonials from './Testimonials.jsx'
+
+export {
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Navbar,
+    Stats,
+    Testimonials
+}
